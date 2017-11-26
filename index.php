21df26e109b5157f8add9ce0fd4ee36f76ba1ee9
tolgacesur <ttolgacesur@gmail.com>
2017-11-26 18:03:53 +0300

    404 error handle and main route design

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\PhpRenderer;
 
 require 'vendor/autoload.php';
 require_once("NobetciEczane.class.php");
 
-$app = new \Slim\App;
+/* 404 ERROR*/
+$c = new \Slim\Container();
+$c['notFoundHandler'] = function ($c) {
+    return function ($request, $response) use ($c) {
+        return $c['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+            ->write('Endpoint not found');
+    };
+};
+
+$app = new \Slim\App($c);
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("./");
 
 $app->get('/', function(Request $request, Response $response){
-    $response->getBody()->write('api yapım aşamasında');
+    return $this->renderer->render($response, "/index.html");
 });
 
 $app->get('/pharmacy/{city}', function (Request $request, Response $response) {
